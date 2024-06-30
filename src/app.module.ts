6d47599d37733eaea5cf85cc4ecc663b96b4b4e1
Matías Véliz Bravo <velizmatias@gmail.com>
2024-06-30 17:56:43 -0400

    feat: serve static files en public folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TmdbModule } from './tmdb/tmdb.module';
-import tmdbConfig from './config/tmdb.config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { configValidationSchema } from './config/config.validation';
+import tmdbConfig from './config/tmdb.config';
+import { TmdbModule } from './tmdb/tmdb.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [tmdbConfig],
       validationSchema: configValidationSchema,
+      load: [tmdbConfig],
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     TmdbModule,
   ],
